545a0e483c20428519829795b2529c0eff7f2480
mandrisic <mirnandrisic@gmail.com>
2024-10-28 23:43:38 +0100

    Updated the design to be responsive with media queries

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,11 +19,11 @@ const NavBar = observer(() => {
     <div className="navbar-container">
         <div className="navbar">
             <h1 className='navbar-title'>LoveBites</h1>
-            <div className="create-icon" onClick={handleNewRecipe}>
+            <div className="create-icon add-recipe" onClick={handleNewRecipe}>
               <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABRUlEQVR4nO2UQU7DMBBFswDWXIDaf7LrCrUqYoNyBBR5Im7AIUBdVeIGnIK20AWnQWLbDYgjgCZxRUhR47Z2JIRHmkVGjt/TeOwkiREjUGRZdmBIjw3htUo9llrSRVwPh4eG9AMTPutZALedwJn0VICG8F4AZwa4KL+hXzqF52lvIPVcqXNnAW60rS2l1XK2JRx6tgZPewP5rgRw41XA1OAGmG+E27XOAolj1OFM+MiBkdQLpU4ZeCvr0DNZ57QhbyFQtR2P3uDbCBT9/pF3uKtAMLiLgMANYdEy7XOB7/TycZsAqXwTXDqzgsvkexcwwMReq7tf4E/SoXKdfYq9CzD0sxW4F4nve47FCu6yz84ChrBcf5B+woMJXKXpiQUupRNyHDITTXgwgUuljkVi333C/NiIKMDxCGjPIWRP+fcEYvy7+AK5mGOrygb74wAAAABJRU5ErkJggg==" />
             </div>
+            <SearchBar />
         </div>
-        <SearchBar />
         <img className='dog' src={dog} alt="dog cooking" />
     </div>
   )
--- src/components/RecipeDetailPage.jsx
+++ src/components/RecipeDetailPage.jsx
@@ -69,8 +69,6 @@ runInAction(() => {
     }
   }
 
-  const hasSteps = Boolean(recipeDetail.strInstructions);
-
   const instructions = recipeDetail.strInstructions
   ? recipeDetail.strInstructions.split(/\r?\n/g).filter(step => step.trim() !== '')
   : [];
@@ -109,16 +107,16 @@ runInAction(() => {
 
   return (
     <div className="details-container">
-      <div className="details-picture">
-          <div className='back details-back' onClick={handleBackToCategory}>
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABI0lEQVR4nO2WS0oDQRCGe69LzQQhdP2VkEUuET3CpLohgjfQrbhWD6R4D9GF+LiFr60mVGeCLu0Z6UbpD2Y59f/1nDGmUPhPCHDore1nEffAkWMsHOMhi4mauedAd2pCmJ7mg8FOMSGlEvjrM1Ez9wQ4F8aNMN6bNWv9CONeY/5I3APOgV67irYy4FWc8RleBC480fSgqjaiyqdxrO3rYYo6UPWqbyFzz3QcK9pJXNGerzM3qcUVx7gN2RNNTWpxRUBvwcBksmkimQPV19rhsdXuu3X/Iw38iriiOx+CDO2uyfE5dsBZM4SXWX5I/Gi0LaCX5nyemBwI80wYH6uS0tVsaPfaDGUnPNtamJ6/n1KTmv3xeEuAUwdc63omN1AomAQsARYcz0MpeHV4AAAAAElFTkSuQmCC" />
-          </div>
-          {id.length > 8 && (
+      <div className='back details-back' onClick={handleBackToCategory}>
+        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABI0lEQVR4nO2WS0oDQRCGe69LzQQhdP2VkEUuET3CpLohgjfQrbhWD6R4D9GF+LiFr60mVGeCLu0Z6UbpD2Y59f/1nDGmUPhPCHDore1nEffAkWMsHOMhi4mauedAd2pCmJ7mg8FOMSGlEvjrM1Ez9wQ4F8aNMN6bNWv9CONeY/5I3APOgV67irYy4FWc8RleBC480fSgqjaiyqdxrO3rYYo6UPWqbyFzz3QcK9pJXNGerzM3qcUVx7gN2RNNTWpxRUBvwcBksmkimQPV19rhsdXuu3X/Iw38iriiOx+CDO2uyfE5dsBZM4SXWX5I/Gi0LaCX5nyemBwI80wYH6uS0tVsaPfaDGUnPNtamJ6/n1KTmv3xeEuAUwdc63omN1AomAQsARYcz0MpeHV4AAAAAElFTkSuQmCC" />
+      </div>
+      {id.length > 8 && (
             <div className="back detail-options" onClick={toggleMenu}>
               <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAABzklEQVR4nO3UXU/aUBzHcV6ITZSnAl2rtxYqz8WrkQKlT7Sg2e2iYSjZWOKFiRuOidt8E5DGvjFp7HkD/91u8RyCmt0s/8/9Lznf9PTEYgghhBBCCCGEEKIyEgnOTKd9O5MmvVyWeIIQHIsiH1tjqCjc8KDgfygVyahSJuNaNfjcaKzdzIwmN9U7/sw0yI1lkrlt3c8dZ+1mo8MbycTK4tPgZHhwc1novxFgIInhQBA41uFPCvnV8ECBUakIZ5UyjGtV+FivhRNVpW4um03ua7u1uu7qMDMNuLEtuHVs+NnrrX44ztaLAzqJuG+kkkAJgKM9aUnbvM/v+6eFPDwJUOswUVXq5krT/GmnDU8C3B7ceR51sxE9vkNYAce7UsQIIKyAT2qdurlqaYQZ0PeifxJwtCs9Pjdg0lAfnxvwy3Opm4104jsB8wvsSQtqgCwH7CtUp26+aFrAvkKu/+IAfXub76aSISXg4Z0oUn+uE1nmTwtySAl4uDg8pG6m+lt+2m6FtJ/4djBIxV79jPKppZ3hIzeXjfpCbsE6/F/PqKIsR8VidFYuReNqZXHOOPyfz+i1ri+/Gd3ou2VGc9tevOoFQgghhBBCCCGE/nO/AbzrQLTLzcbVAAAAAElFTkSuQmCC" />
             </div>
           )}
-          <img className='details-img' src={recipeDetail.strMealThumb} alt={recipeDetail.strMeal} />
+      <div className="details-picture">
+        <img className='details-img' src={recipeDetail.strMealThumb} alt={recipeDetail.strMeal} />
       </div>
       <div className="details">
         <h2>{recipeDetail.strMeal}</h2>
@@ -131,7 +129,7 @@ runInAction(() => {
           </div>
         </div>
         <h3>Ingredients:</h3>
-        <div className="ingredients-container">
+        <div className="ing-container">
           <div className='ingredients-list'>
           {ingredientsList.map((item, index) => (
             <div key={index} className="ingredient-item">
